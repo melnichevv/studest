35ca71c461e9af0fe44685e138fe1fecf25c8ee5
Vladimir Melnichenko <melnichevv@gmail.com>
2018-04-03 22:37:36 +0300

    Added new frontend dir with React, Redux, Webpack, Apollo, etc

---
+++ frontend_new/src/js/Root.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { HashRouter as Router } from 'react-router-dom';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { BatchHttpLink } from "apollo-link-batch-http/lib/index";
+import ApolloProvider from "react-apollo/ApolloProvider";
+
+const middlewareLink = setContext(() => ({
+  headers: {
+    authorization: `JWT ${localStorage.getItem("token")}` || ""
+  }
+}));
+const httpLink = new BatchHttpLink({
+  uri: "http://0.0.0.0:8000/gql",
+  credentials: "same-origin"
+});
+
+const client = new ApolloClient({
+  link: middlewareLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
+
+export default class Root extends Component {
+  get content() {
+    return (
+      <Router>
+        {this.props.routes}
+      </Router>
+    );
+  }
+
+  render() {
+    return (
+      <ApolloProvider client={client} store={this.props.store}>
+        {this.content}
+      </ApolloProvider>
+    );
+  }
+}
+
+Root.propTypes = {
+  routes: PropTypes.element.isRequired,
+  store: PropTypes.object.isRequired,
+};
